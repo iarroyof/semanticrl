6f0b3ca54c82cb2ad4a73e7bde49431851956b20
vector_lap <iarroyof@yandex.com>
2019-04-20 22:13:22 -0500

    fixed the issue of hangging with input files smaller than sample size

--- language_env.py
+++ language_env.py
@@ -11,11 +11,11 @@ from pdb import set_trace as st
 
 def log2(x):
     return 0.0 if x <= 0.0 else lg2(x)
-       
+
 
 class stream(object):
-    def __init__(self, size, input_data, tokenizer=None, 
-                        token_pattern=r"(?u)\b\w\w+\b", 
+    def __init__(self, size, input_data, tokenizer=None,
+                        token_pattern=r"(?u)\b\w\w+\b",
                         give_strings=True, lower_case=True):
         self.input_data = input_data
         self.size = size
@@ -37,16 +37,16 @@ class stream(object):
         except OSError:
             self.isfile = False
             pass
-            
+
         if size % 2 == 0:
             self.size = int(size + 1)
         else:
             self.size = int(size)
-            
+
         if self.tokenizer is None:
             token_pattern = re.compile(self.token_pattern)
             self.tokenizer = lambda l: token_pattern.findall(l)
-        
+
 
     def tokenize(self, str_list):
         assert isinstance(str_list, str)  # The input must be a raw string
@@ -54,41 +54,49 @@ class stream(object):
                                             if self.lower_case else str_list)
         return self
 
-      
+
     def winds(self):
-        
-        itrs = it.tee(self.words, self.size)
+        itrs = it.tee(self.words, len(self.words) 
+                                if self.size > len(self.words) else self.size)
         windows = [it.islice(anItr, s, None) for s, anItr in enumerate(itrs)]
         self.windows = list(zip(*windows))
-        
+
         return self
 
 
     def __iter__(self):
+
         if self.isfile:
             self.buff = []
+            eof = True
             for line in self.input_file:
-                self.tokenize(' '.join(self.buff) + ' ' + line)
-                if len(self.words) < self.size:
-                    wind_complete = False
-                    while not wind_complete:
-                        self.buff += self.words
-                        self.tokenize(self.input_file.readline())
-                        if len(self.buff + self.words) >= self.size:
-                            wind_complete = True
-                    self.words = (self.buff + self.words)[:self.size]
-                    
-                self.buff = (self.buff + self.words)[self.size:]
+                self.tokenize(line)
+                self.buff += self.words
+                if len(self.buff) < self.size and not line is None:
+                    continue
+                    eof = True
+                else:
+                    self.words = (self.buff)[:self.size]
+                    self.buff = (self.buff)[self.size:]
+                    self.winds()
+                    for w in self.windows:
+                        yield ' '.join(w) if self.give_strings else w
+                    eof = False
+
+            if eof:
+                self.words = self.buff[:self.size]
+                self.buff = self.buff[self.size:]
                 self.winds()
                 for w in self.windows:
-                    yield ' '.join(w) if self.give_strings else w    
+                    yield ' '.join(w) if self.give_strings else w
         else:
             try:
                 self.words
             except AttributeError:
                 self.tokenize(self.input_data)
-                
+
             self.winds()
+            
             for w in self.windows:
                 yield ' '.join(w) if self.give_strings else w
 
@@ -98,23 +106,25 @@ class language_env(object):
         self.input_data = input_data
         self.entropy_norm_size = float(entropy_norm_size)
         self.tuple_size = tuple_size
-        self.samples = stream(size=self.entropy_norm_size, 
+        self.samples = stream(size=self.entropy_norm_size,
                                             input_data=self.input_data)
         self.actions = [False] * (self.tuple_size + 1)
-                        # Words selected from the tuple plus if-relation decision 
+                        # Words selected from the tuple plus if-relation decision
         self.zeta = zeta
-        
+
+
     def P(self, w, tuples):
         """ P(X=w) """
         #return sum([1 for t in tuples if w in t])/self.entropy_norm_size
         return sum(tuples, ()).count(w) / self.entropy_norm_size
-    
+
+
     def condP(self, a, b, tuples):
-        """ P(a|b) = P(A=a)P(B=b)/P(B=b) """ 
+        """ P(a|b) = P(A=a)P(B=b)/P(B=b) """
         Pa = self.P(a, tuples)
         Pb = self.P(b, tuples)
         return Pa * Pb / Pb
-    
+
 
     def entropy_norm(self, tuples):
         vocab = set(sum(tuples, ()))
@@ -144,28 +154,27 @@ class language_env(object):
         #query = list(compress(query, self.actions[:-1]))
         I_D = []  # I(D; q) =  H(D) - H(D|q)
         I_as = H_D  # I(D; q1, q2,...,qn) =  H(D) - H(D|q1) - ... - H(D|qn)
-            
+
         for q in query:
-            P_wq = odict({w + '|' + q: self.condP(w, q, tuples) 
+            P_wq = odict({w + '|' + q: self.condP(w, q, tuples)
                                                             for w in vocab})
-            H_Dq = -P_[q] * sum(P_wq[pwq] * log2(P_wq[pwq]) 
+            H_Dq = -P_[q] * sum(P_wq[pwq] * log2(P_wq[pwq])
                                                     for pwq in P_wq.keys())
-            I_D.append((q, H_D - H_Dq)) 
+            I_D.append((q, H_D - H_Dq))
             I_as -= H_Dq
         # TODO: define a,b,c for giving negative reward
         # if not (mmi_norm(a,b, tuples) > mmi_norm(b,c, tuples) > mmi_norm(a,c, tuples)):
-        #       I_as = self.zeta 
+        #       I_as = self.zeta
         return I_D, I_as
 
 
     def __iter__(self):
         init = 0
-        
         for sample in self.samples:
-            self.tuples = stream(size=self.tuple_size, input_data=sample, 
+            self.tuples = stream(size=self.tuple_size, input_data=sample,
                                                             give_strings=False)
             if init < self.entropy_norm_size / 2:
-                for t in self.tuples: 
+                for t in self.tuples:
                     tup = list(compress(t, self.actions[:-1]))
                     if init < self.entropy_norm_size / 2:
                         init += 1
@@ -181,51 +190,51 @@ class language_env(object):
 
 
 input_data = "LICENSE"
-t_size = 3
+s_size = 200
+t_size = 5
 N = 100
 mi_ = []
 Ias = []
 ws_ = []
 
-env = language_env(input_data=input_data, entropy_norm_size=150, tuple_size=t_size)
+env = language_env(input_data=input_data, entropy_norm_size=s_size, tuple_size=t_size)
 while True:
-    actions = np.random.choice([0, 1], 
-                                 size=(t_size + 1,), 
+    actions = np.random.choice([0, 1],
+                                 size=(t_size + 1,),
                                  p=np.random \
-                                     .dirichlet(alpha=[1, 1], 
+                                     .dirichlet(alpha=[1, 1],
                                                 size=1)[0].tolist()).tolist()
     if np.nonzero(actions)[0].shape[0] > 2:
         break
 
 env.set_actions(actions)
-
+#st()
 for i, s in enumerate(env):
     ws_.append(" ".join(tuple(odict(s[0]))))
     mi_.append(np.std(list(odict(s[0]).values()))) #[int(1 + t_size / 2)])
     Ias.append(s[1])
-    
-    if len(s[0]) == 1: 
-        continue
-    elif len(s[0]) == 2: 
+
+    if len(s[0]) == 2:
+        #print(s)
         env.set_actions([1] * (t_size + 1))
-    else:    
+    else:
     # TODO: Incorporate direction of the relation (order of actions 1,2,3),
-    # however, how to group words so as to form noun prhases ordered from 
+    # however, how to group words so as to form noun prhases ordered from
     # agent to pacient? The easiest way is to learn parameters of the dis-
     # tributions chosing integers {1,2,3} with repetitions, where repeated
     # numbers form groups. But, this controls directly the grouping action?
         while True:
             actions = np.random \
-                        .choice([0, 1], 
-                              size=(t_size + 1,), 
+                        .choice([0, 1],
+                              size=(t_size + 1,),
                               p=np.random \
-                                  .dirichlet(alpha=[1, 1], 
+                                  .dirichlet(alpha=[1, 1],
                                              size=1)[0].tolist()).tolist()
             if np.nonzero(actions)[0].shape[0] > 2:
                 break
     env.set_actions(actions)
-    print(np.nonzero(actions)[0].shape[0], s)        
-    if i > N > 0: break 
+
+    if i > N > 0: break
 
 info = pd.DataFrame({"tuple": ws_, "MI_avg": mi_, "Ias": Ias})
 
