daf297de9773d626c580208f347b7817682bd13b
Nacho <iaf@ccg.unam.mx>
2019-09-18 21:48:32 -0500

    Figures fixed for corrected mutual informations

--- figures/plot_mi.py
+++ figures/plot_mi.py
@@ -1,13 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib                                            
+matplotlib.use                                                         
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import argparse
 
-def plot_mis(csv_file, comparing_cols, title):
-    df = pd.read_csv(csv_file)
+def plot_mis(csv, comparing_cols, title, roll=20):
+    df = pd.read_csv(csv) \
+           .replace(np.inf, np.NaN) \
+           .interpolate(method ='linear')
     colors = ['b','g','r','c','m','y','k','w','burlywood']
     plt.figure()
     ax = plt.subplot(111) 
     for column, color in zip(comparing_cols, colors):
         col = df[column]
-        ma = col.rolling(20).mean()
-        mstd = col.rolling(20).std()
+        ma = col.rolling(roll).mean()
+        mstd = col.rolling(roll).std()
         plt.plot(ma.index, ma, color, label=column)
         plt.fill_between(mstd.index, ma - 2 * mstd, ma + 2 * mstd,
                      color=color, alpha=0.2)
@@ -16,3 +25,15 @@ def plot_mis(csv_file, comparing_cols, title):
         plt.ylabel('Bits')
         ax.legend()
     plt.show()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cols", help="Comparing columns to plot "
+					"(spaced strings, column names).",
+                    type=str, nargs='+')
+parser.add_argument("--in_csv", help="Input csv file name")
+parser.add_argument("--title", help="Title of the figure")
+args = parser.parse_args()
+
+
+plot_mis(csv=args.in_csv, comparing_cols=args.cols, title=args.title)
