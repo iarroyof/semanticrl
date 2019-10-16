6295b31eee3a8ec74907ee006f6443875a44dd7b
Ignacio Arroyo <axayakac@gmail.com>
2019-10-16 18:10:56 -0500

    Plotting stript fixed and with needed input parameters

--- figures/plot_mi.py
+++ figures/plot_mi.py
@@ -2,21 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib
 from matplotlib import rc
-matplotlib.use                                                         
+matplotlib.use
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
 import argparse
+from pdb import set_trace as st
 
 
 def plot_mis(csv, comparing_cols, title, roll=20):
     df = pd.read_csv(csv) \
            .replace(np.inf, np.NaN) \
-           .interpolate(method ='linear')
+           .interpolate(method='linear')
     colors = ['b','g','r','c','m','y','k','w','burlywood']
     plt.figure()
-    ax = plt.subplot(111) 
+    ax = plt.subplot(111)
     for column, color in zip(comparing_cols, colors):
         col = df[column]
         ma = col.rolling(roll).mean()
@@ -33,11 +34,14 @@ def plot_mis(csv, comparing_cols, title, roll=20):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--cols", help="Comparing columns to plot "
-					"(spaced strings, column names).",
+					"(semicolon spaced strings, being column names. "
+                    "Special characters like '$' must be scaped.)",
                     type=str, nargs='+')
 parser.add_argument("--in_csv", help="Input csv file name")
 parser.add_argument("--title", help="Title of the figure")
+parser.add_argument("--roll", help="Rolling mean window (default: 20).",
+                    type=int)
 args = parser.parse_args()
 
-
-plot_mis(csv=args.in_csv, comparing_cols=args.cols, title=args.title)
+plot_mis(csv=args.in_csv, comparing_cols=args.cols, title=args.title,
+        roll=args.roll)
