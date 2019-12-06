3e22dbc7a4d0da1fa2b9aec323ac9bfdd6bc3354
Nacho <iaf@ccg.unam.mx>
2019-12-05 20:47:35 -0600

    version including all hyperparameters in command line. Added softmax density and corrected gaussian

--- srl_env_mi_tests.py
+++ srl_env_mi_tests.py
@@ -1,4 +1,4 @@
-
+from functools import partial
 from jellyfish import damerau_levenshtein_distance as dlbsh
 from srl_env_v01 import textEnv
 import itertools
@@ -26,7 +26,8 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 ANALYZER = 'char'
 STRUCTCOLS = ['X', 'Y', 'Z']
 COMBINATIONS= ['Y+Z', 'X+Z', 'X+Y']
-BACKEND = 'lk'
+BACKEND = 'mp'
+
 
 def dot_distance(A, B, binary=True, euclid=False, ngramr=(1, 3)):
     """ This method computes either Euclidean or Hamming distance between
@@ -64,8 +65,10 @@ def set_valued_gaussian(S, M, sigma=5.0, metric='hmm', ngramr=(1, 3)):
         assert isinstance(S, str) and isinstance(M, str)
         distance = dlbsh(S, M)
     elif metric == 'hmm':
-        distance = dot_distance(S, M, binary=True,
-                                            euclid=False, ngramr=ngramr)
+        A = set(analyzer(S))
+        B = set(analyzer(M))
+        # |Symmetric difference| == sum(xor)
+        distance = len(A.union(B) - A.intersection(B))
     elif metric == 'euc':
         distance = dot_distance(S, M, binary=False,
                                             euclid=True, ngramr=ngramr)
@@ -77,10 +80,50 @@ def set_valued_gaussian(S, M, sigma=5.0, metric='hmm', ngramr=(1, 3)):
                                              -distance ** 2 / (2 * sigma ** 2))
 
 
-@delayed
-@wrap_non_picklable_objects
+def gausset(S, ngramr=(1, 3), sigma=5.0):
+    S = list(S)
+    s = analyzer(S[0])
+    hit_miss = [analyzer(m) for m in S[1:]]
+    measure = lambda a, b: len(set(a).union(b) - set(a).intersection(b))
+    likelihood = lambda filter_trap: sum([math.exp(
+                                            -measure(filter_trap, hm)**2/
+                                                               2*sigma**2)
+                                                          for hm in hit_miss])
+    rlh = likelihood(s)
+    evidence = [likelihood(s) for s in hit_miss]
+    evidence.append(rlh)
+    return rlh/sum(evidence)
+                                                                
+
+def setmax(S, bias=None, ngramr=(1, 3), sigma=1.0):
+    """This function takes a row 'S' where the fisrt item S[0] is the string
+    we want to know its probability, with respect to the reamining items.
+    The returned value es the needed Bayesian probability (a real number
+    in [0, 1]). This is the Boltzman (softmax) distribution defined on 
+    Linguistic Random Sets (setmax).
+    """
+    S = list(S)
+    s = analyzer(S[0])
+    hit_miss = [analyzer(m) for m in S[1:]]
+    # We need to specify the sign given cardinalities are always positive
+    measure = lambda a, b: len(set(a).intersection(b))
+                  #if comparer == 'intersect' else len(set(a).union(b)
+                  #                                - set(a).intersection(b)))
+    likelihood = lambda filter_trap: sum([math.exp(
+                                            -sigma * measure(filter_trap, hm))
+                                                            for hm in hit_miss])
+    # Likelihood
+    rlh = likelihood(s)
+    # Evicence
+    evidence = [likelihood(s) for s in hit_miss]
+    evidence.append(rlh)
+    return rlh/sum(evidence)
+
+
+#@delayed
+#@wrap_non_picklable_objects
 def compute_set_probability(Akdf, prod_cols, hit_miss_samples=50, sigma=5.0,
-                            metric='hmm', ngramr=(1, 3)):
+                            metric='hmm', ngramr=(1, 3), density='setmax'):
     try:
         assert len(Akdf.index) >= hit_miss_samples, (
                                                 "The number of hit-and-miss"
@@ -88,6 +131,11 @@ def compute_set_probability(Akdf, prod_cols, hit_miss_samples=50, sigma=5.0,
                                                 " than the number of samples")
     except AssertionError:
         return None
+    
+    if density == 'gausset':
+        capacity = partial(gausset, ngramr=ngramr, sigma=sigma)
+    if density == 'setmax':
+        capacity = partial(setmax, ngramr=ngramr, sigma=sigma)
     
     joints = []
     for d in prod_cols:
@@ -103,17 +151,31 @@ def compute_set_probability(Akdf, prod_cols, hit_miss_samples=50, sigma=5.0,
                                                           axis=1)
     for a, b in prod_cols:  # This lists already contains joints
         measures = []
-        for _ in range(hit_miss_samples):
-            B = Akdf[b].sample(frac=1)
-            measures.append(
-                np.vectorize(set_valued_gaussian,
-                                 excluded=set(['ngramr']))(
-                        Akdf[a].str.lower(), B.str.lower(), sigma=sigma,
-                        metric=metric, ngramr=ngramr
-                        )
-                )
+        #for _ in range(hit_miss_samples):
+        #    B = Akdf[b].sample(frac=1)
+        #    measures.append(
+        #        np.vectorize(set_valued_gaussian,
+        #                         excluded=set(['ngramr']))(
+        #                Akdf[a].str.lower(), B.str.lower(), sigma=sigma,
+        #                metric=metric, ngramr=ngramr
+        #                )
+        #        )
+        rdns = ["b_" + str(i) for i in range(hit_miss_samples)]
+        dupst = np.array([Akdf[b].values] * hit_miss_samples)
+        _ = np.apply_along_axis(np.random.shuffle, 1, dupst)
+        rdns_df = pd.DataFrame(dupst.T, columns=rdns)
+        trap_df = Akdf[[a, b]]
+        if a == b:
+            trap_df.set_axis([a, b + '_'], axis=1, inplace=True)
+            
+        to_operate = pd.concat([trap_df.reset_index(drop=True),
+                                rdns_df.reset_index(drop=True)],
+                                axis=1, sort=False)[[a] + rdns]
         joincol = "$\mathcal{{N}}\{{h(" + ', '.join((a, b)) + "), \sigma\}}$"
-        Akdf[joincol] = np.vstack(measures).mean(axis=0)
+        
+        Akdf[joincol] = to_operate.apply(capacity, axis=1).tolist()
+        
+        #Akdf[joincol] = np.vstack(measures).mean(axis=0)
     return Akdf.dropna()
 
 
@@ -124,8 +186,8 @@ def rdn_partition(state):
     return {c: " ".join(a) for c, a in zip(STRUCTCOLS, action)} 
 
 # TODO: Verify how wrapping decorators work as they seem to be faster
-@delayed
-@wrap_non_picklable_objects
+#@delayed
+#@wrap_non_picklable_objects
 def compute_mutuals(df, cols):
     scs = ''.join(STRUCTCOLS)
     patt = r"\{{h\(([{0}]\+?[{0}]?), ([{0}]\+?[{0}]?)\)" \
@@ -160,7 +222,7 @@ def compute_mutuals(df, cols):
 
 
 def compute_mi_steps(Akdf, out_csv, metric='hmm', sigma=5.0, prod_cols=None,
-                                              n_hit_miss=50, ngramr=(1, 3)):
+                              density='setmax', n_hit_miss=50, ngramr=(1, 3)):
     """ This method calls 'compute_set_probability()' 
     """
     probcs = []
@@ -185,7 +247,7 @@ def compute_mi_steps(Akdf, out_csv, metric='hmm', sigma=5.0, prod_cols=None,
         P_Aks = Parallel(n_jobs=NJOBS)(
                     delayed(compute_set_probability)(
                         A_k, prod_cols=prod_cols, hit_miss_samples=n_hit_miss,
-                                     metric=metric, sigma=sigma, ngramr=ngramr)
+                        density=density, metric=metric, sigma=sigma, ngramr=ngramr)
                                                             for A_k in A_tau)
         logging.info("Estimated set probabilities in {}s..." \
                         .format(time.time() - t))
@@ -194,7 +256,7 @@ def compute_mi_steps(Akdf, out_csv, metric='hmm', sigma=5.0, prod_cols=None,
         t = time.time()
         info_steps = Parallel(n_jobs=NJOBS)(
                     delayed(compute_mutuals)(df, probcs)
-                                           for df in P_Aks if not df is None)
+                             for df in P_Aks if not (df is None or df.empty))
         logging.info("Estimated MIs in {}s..." \
                                 .format(time.time() - t))
                                 
@@ -232,7 +294,10 @@ parser.add_argument("--sample", help=("Sample size for text environment steps"
 parser.add_argument("--nsteps", help=("Number of steps to simulate"
                                       " (default: 50)"),
                     type=int, default=50)
-parser.add_argument("--bw", help=("Bandwidth for the kernel estimator."
+parser.add_argument("--density", help=("Density function/kernel estimator."
+                                  " ('gausset', 'setmax'; default: 'gausset')"),
+                                  default='gausset')
+parser.add_argument("--bw", help=("Bandwidth/strinctness for the kernel estimator."
                                       " (default: 5.0)"),
                     type=float, default=5.0)
 parser.add_argument("--hitmiss", help=("Number of samples to build the"
@@ -259,6 +324,8 @@ N_STEPS = args.nsteps  # 120  #e.g.: n_input_oie/SAMPLE_SIZE=12167/100=121.67
 ngramr = tuple(args.ngrams)  # (1, 3)
 t_size = args.wsize  # 10
 N_TRAJECTORIES = 2
+analyzer = CountVectorizer(analyzer=ANALYZER, ngram_range=ngramr)\
+                                .build_analyzer()
 
 if args.hitmiss == 0:
     n_hit_miss = int(SAMPLE_SIZE * 0.25)  # 25% of the sample size
@@ -292,7 +359,7 @@ gsAkdf = gsAkdf[
 logging.info("Computing MI for OpenIE actions...")
 
 compute_mi_steps(gsAkdf, prod_cols=TOANALYZE, out_csv=input_oie.split('.')[0],
-                 metric=args.metric, sigma=args.bw,
+                 metric=args.metric, sigma=args.bw, density=args.density,
                     ngramr=ngramr, n_hit_miss=n_hit_miss)
 
 # Create text environment sampler to simulate random actions
@@ -315,7 +382,7 @@ for t in range(N_STEPS):
 rdn_Akdf = pd.DataFrame(sum(A, []))
 
 logging.info("Computing MI for random actions...")
-compute_mi_steps(rdn_Akdf, prod_cols=TOANALYZE,
+compute_mi_steps(rdn_Akdf, prod_cols=TOANALYZE, density=args.density,
                     out_csv=input_plain.split('.')[0],
                     metric=args.metric, sigma=args.bw,
                     ngramr=ngramr, n_hit_miss=n_hit_miss)
