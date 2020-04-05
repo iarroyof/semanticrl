c44d189d66602b325b84bff2b77858acef22fc2f
Nacho <nacho@bisnaga.net>
2020-04-05 13:16:55 -0500

    Test loop object coded. tests pending.

--- test_loop.py
+++ test_loop.py
@@ -1,4 +1,7 @@
+from srl_vs_rdn import SrlEnvTest
 import random
+
+NACTIONS = 28561  # Number of lines in openIE input file.
 
 def rdn_range(a, b, N=10):
     """
@@ -26,3 +29,31 @@ def rdn_range(a, b, N=10):
 
     for tup in random.sample(possible, N):
         yield tup
+
+
+def test_settings(n_tests):
+    """
+    bias=3, hitmiss=0.3, bw=5.0, density='expset', ngrams=(1, 3)
+    """
+    
+
+def main():
+    in_text = "data/dis_train_.txt"
+    in_open = "data/dis_train.txt.oie"
+    rdn_win = 10
+    sampran = 500
+    n_steps = 30
+    samples = int(sampran * 0.3) 
+    
+    
+    
+    for s in random.sample(range(10, sampran), samples):
+        nsteps = int(NACTIONS/s)
+        out_dir = "results_" + str(s) + "_samples"
+        test = SrlEnvTest(in_oie=in_open, in_txt=in_text, output_dir=out_dir,
+                        wsize=rdn_win, sample=s, nsteps=nsteps)
+        for param in settings:
+            test.fit(bias=3, hitmiss=0.3, bw=5.0, density='expset', ngrams=(1, 3),
+                                                                        output=None)
+            
+                    
