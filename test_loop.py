89eaaf4a4f70a48e93a8513a4214b6f2dc734169
Nacho <nacho@bisnaga.net>
2020-04-11 19:58:19 -0500

    discretizados los intervalos. falta discretizarlos a una cantidad de pasos fija a todos

--- test_loop.py
+++ test_loop.py
@@ -25,14 +25,14 @@ def rand_ranges(a, b, N):
 
 
 def test_settings(n_tests, steps=10, stepper=1.0, ranges=dict(
-        bias=(0.0, 10.0),
-        hitmiss=(0.1, 1.0),
-        bw=(1.0, 5.0),
+        bias=(0.0, 10.0),  # 10
+        hitmiss=(0.1, 1.0),  # 10
+        bw=(1.0, 5.0),  # 4
         # ['gausset', 'setmax', 'expset']
-        density=('expset', 'gausset'),
-        ngrams={'low': 1, 'high': 5} )):
+        density=('expset', 'gausset'), # 2
+        ngrams={'low': 1, 'high': 5} )): # 2 ** 5
     """
-    Generate random parameters:
+    Generate random parameters, e.g.:
         bias=3.0, hitmiss=0.3, bw=5.0,
         density='expset', ngrams={'low': 1, 'high': 5}
     """
@@ -43,20 +43,19 @@ def test_settings(n_tests, steps=10, stepper=1.0, ranges=dict(
                 if v[1] > stepper:
                     inter = int(v[1] - v[0])
                     settings[p] = np.random.choice(
-                                    np.linspace(v[0], v[1], inter),
+                                    np.round(np.linspace(v[0], v[1], inter)),
                                     n_tests, p=[1.0/inter] * inter)
                 else:
                     settings[p] = np.random.choice(
                                             np.linspace(v[0], v[1], steps),
                                                 n_tests, p=[1.0/steps] * steps)
-                
             else:
                 settings[p] = np.random.choice(v, n_tests,
                                                p=[1.0/len(v)] * len(v)).tolist()
         else:
             settings[p] = rand_ranges(v['low'], v['high'], N=n_tests)
 
-    for _, s in pd.DataFrame(settings).iterrows():
+    for _, s in pd.DataFrame(settings).drop_duplicates().iterrows():
         yield dict(s.items())
 
 from pdb import set_trace as st
@@ -65,17 +64,19 @@ def main():
     in_open = "data/dis_train.txt.oie"
     rdn_win = 8  # 6 + 2 (mean + std)
     sampran = (10, 300)  # sample range
-    samples = int((sampran[1] - sampran[0]) * 0.1)
-    n_fit_params = 5 - 1  # all - ngrams
-    # (0 -100), (0 - 1), (0 - 5), (expset, gausset), (1, 5)
-    max_tests_possible =  100 *10 * 50 * 2 * (2 ** 5) # (sampran[1] - sampran[0]) * n_fit_params * (2 ** 5)
-    n_tests = int(max_tests_possible * 0.1) # 13920
+    #samples = int((sampran[1] - sampran[0]) * 0.1)  # 290 * 0.1 = 29
+    samples = list(range(10, 320, 50))
+    # (0. - 10.), (.1 - 1.), (1. - 5.), (expset, gausset), (1, 5)
+    max_tests_possible =  10 * 10 * 4 * 2 * (2 ** 5)
+    n_tests = int(max_tests_possible * 0.15)  # 25600 * 0.1 = 2560
 
     st()
     settings = test_settings(n_tests=n_tests)
     logging.info("Performing {} experiments" \
-                                    .format(samples * n_tests))
-    for s in random.sample(range(sampran[0], sampran[1]), samples):
+                                    .format(samples * n_tests)) # 29 * 2560
+    # = 222720 minutos a dias --> 154.66 días --> 5 meses
+    # for s in random.sample(range(sampran[0], sampran[1]), samples):
+    for s in samples:
         nsteps = int(float(NACTIONS)/float(s))
         out_dir = ("/almac/ignacio/results_srl_env/wsize-"
                                 + str(rdn_win) + "/sample-" + str(s))
