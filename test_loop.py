dbd66ee24f76c9708c730d39b76c3dc18abe1ad7
Nacho <nacho@bisnaga.net>
2020-04-11 20:41:56 -0500

    discretizados los intervalos. se redujo el tiempo a 15 días máximo

--- test_loop.py
+++ test_loop.py
@@ -24,7 +24,7 @@ def rand_ranges(a, b, N):
     return ranges
 
 
-def test_settings(n_tests, steps=10, stepper=1.0, ranges=dict(
+def test_settings(n_tests, steps=5, ranges=dict(
         bias=(0.0, 10.0),  # 10
         hitmiss=(0.1, 1.0),  # 10
         bw=(1.0, 5.0),  # 4
@@ -40,42 +40,38 @@ def test_settings(n_tests, steps=10, stepper=1.0, ranges=dict(
     for p, v in ranges.items():
         if not isinstance(v, dict):
             if not isinstance(v[0], str):
-                if v[1] > stepper:
-                    inter = int(v[1] - v[0])
-                    settings[p] = np.random.choice(
-                                    np.round(np.linspace(v[0], v[1], inter)),
-                                    n_tests, p=[1.0/inter] * inter)
-                else:
-                    settings[p] = np.random.choice(
-                                            np.linspace(v[0], v[1], steps),
-                                                n_tests, p=[1.0/steps] * steps)
+                settings[p] = np.random.choice(
+                                        np.round(np.linspace(v[0], v[1], steps)),
+                                        n_tests, p=[1.0/steps] * steps)
             else:
                 settings[p] = np.random.choice(v, n_tests,
                                                p=[1.0/len(v)] * len(v)).tolist()
         else:
             settings[p] = rand_ranges(v['low'], v['high'], N=n_tests)
 
-    for _, s in pd.DataFrame(settings).drop_duplicates().iterrows():
-        yield dict(s.items())
+    sdf = pd.DataFrame(settings).drop_duplicates()
+    sdf = sdf.reset_index()
+    last = sdf.index[-1]
+    for i, s in sdf.iterrows():
+        yield (i, last, dict(s.items()))
 
 from pdb import set_trace as st
 def main():
     in_text = "data/dis_train_.txt"
     in_open = "data/dis_train.txt.oie"
     rdn_win = 8  # 6 + 2 (mean + std)
-    sampran = (10, 300)  # sample range
-    #samples = int((sampran[1] - sampran[0]) * 0.1)  # 290 * 0.1 = 29
-    samples = list(range(10, 320, 50))
+    sampran = (10, 320)  # sample range
+    max_ngrams = 5
+    range_steps = 5
+    samples = list(range(*sampran, 50))
     # (0. - 10.), (.1 - 1.), (1. - 5.), (expset, gausset), (1, 5)
-    max_tests_possible =  10 * 10 * 4 * 2 * (2 ** 5)
-    n_tests = int(max_tests_possible * 0.15)  # 25600 * 0.1 = 2560
+    max_tests_possible =  (range_steps ** 3) * 2 * (2 ** max_ngrams)
+    n_tests = int(max_tests_possible * 0.3)  # 25600 * 0.3 = 2560
 
+    settings = test_settings(n_tests=n_tests, steps=range_steps)
+    logging.info("Performing {} experiments in {} minutes (approx.)" \
+                       .format(samples * n_tests, samples * n_tests * 3))
     st()
-    settings = test_settings(n_tests=n_tests)
-    logging.info("Performing {} experiments" \
-                                    .format(samples * n_tests)) # 29 * 2560
-    # = 222720 minutos a dias --> 154.66 días --> 5 meses
-    # for s in random.sample(range(sampran[0], sampran[1]), samples):
     for s in samples:
         nsteps = int(float(NACTIONS)/float(s))
         out_dir = ("/almac/ignacio/results_srl_env/wsize-"
@@ -83,10 +79,11 @@ def main():
         if not path.exists(out_dir):
             makedirs(out_dir)
         test = SrlEnvTest(in_oie=in_open, in_txt=in_text, output_dir=out_dir,
-                        wsize=rdn_win, sample=s, nsteps=nsteps)
+                            wsize=rdn_win, sample=s, nsteps=nsteps)
         logging.info("Window and sample: {}".format(out_dir))
-        for param in settings:
-            logging.info("Parameters test: {}".format(param))
+        for i, l, param in settings:
+            logging.info("Parameters test {} ({:.1f} %%): {}" \
+                        .format(i, (i / l) * 100, param))
             test.fit(**param)
 
 
