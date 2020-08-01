de17ce13529d79ad02eb12a6b14790345f525f3d
Nacho <nacho@bisnaga.net>
2020-08-01 18:54:59 -0500

    latest version of plot_rewards.py and rewards matrix plot

--- utils/plot_params.py
+++ utils/plot_params.py
@@ -2,7 +2,9 @@ import pandas as pd
 import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import warnings
+import sys
 
 from pdb import set_trace as st
 
@@ -14,40 +16,52 @@ def map_measures(m):
         'HXY_HYZ_HXZ': 'Entropy'
     }
     return mapp[m]
-    
 
-df = pd.read_csv("results/train_results_originalExtremaWeighted"
-                "GaussianBayesianExpBeta1e8Pvalues_rwd.csv")
+
+infile = sys.argv[1]
+#df = pd.read_csv("results/train_results_originalExtremaWeighted"
+#                "GaussianBayesianExpBeta1e8Pvalues_rwd.csv")
+drop_rwd = 'Reward'
+nng = 1
+ylog = False
+palette = 'Dark2' #'tab10'
+ptte_start = 3
+gauss_start = 3
+
+df = pd.read_csv(infile)
 
 df['Measure'] = df['Measure'].apply(map_measures)
-colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',   
-             'tab:brown', 'tab:pink']
-drop_rwd = 'Reward'
+colors = plt.get_cmap(palette).colors
 rwdt = 'pReward' if drop_rwd == 'Reward' else 'Reward'
 measures = df.Measure.unique()
 categories = ['Agent',
-              #'density',
               'ngrams']
-df = df[df.density=='expset']
 numerics = ['Sample', 'bias', 'bw']
 to_drop = ['wsize', drop_rwd, 'hitmiss',
            'ABpvalue', 'BCpvalue', 'CApvalue']
-nng = 2
-ylog = False
+
 df = df.drop(to_drop, axis=1)
 
 fig, axs = plt.subplots(measures.shape[0], len(numerics), figsize=(8, 20))
-#fig.set_figheight(50)
-#fig.set_figwidth(20)
-
-for i, mea in enumerate(measures):
+for density in ['expset', 'gausset']:
+  for i, mea in enumerate(measures):
+    
     best_params = dict(
-        df[(df.Measure==mea) & (df.Agent=='oie')].sort_values(
+        df[(df.Measure==mea) &
+           (df.Agent=='oie') &
+           (df.density==density)].sort_values(
+                rwdt, ascending=False).drop(
+                    [rwdt, 'Measure'], axis=1).iloc[0].items())
+   
+    print(f"Best hyperparams for {mea}: {best_params}")
+
+    rank_ngrams = df[
+        (df.Measure==mea) &
+        (df.Agent=='oie') &
+        (df.density==density)].sort_values(
             rwdt, ascending=False).drop(
-                [rwdt, 'Measure'], axis=1).iloc[0].items())
-    rank_ngrams = df[(df.Measure==mea) & (df.Agent=='oie')].sort_values(
-        rwdt, ascending=False).drop(
-            [rwdt, 'Measure'], axis=1).ngrams
+                [rwdt, 'Measure'], axis=1).ngrams
+
     rank_ngrams = list(dict.fromkeys(rank_ngrams).keys())
     n_grams = list(set(rank_ngrams))
     best_ngrams = []
@@ -60,14 +74,19 @@ for i, mea in enumerate(measures):
                 n_grams.remove(n)
             except:
                 break
-    #best_ngrams = best_ngrams[:nng]
-    best_ngrams = [best_ngrams[0], best_ngrams[-1]]
+    if nng == 2:
+        best_ngrams = [best_ngrams[0], best_ngrams[-1]]
+    elif nng == 1:
+        best_ngrams = [best_ngrams[0]]
+    elif nng > 2:
+        best_ngrams = best_ngrams[:nng]
     
-    mdf = df[df.Measure==mea].drop('Measure' , axis=1)
+    mdf = df[(df.Measure==mea) & (df.density==density)].drop('Measure' , axis=1)
     axs[i, 0].set_ylabel(mea)
     for j, num in enumerate(numerics):
+        color = ptte_start if density.startswith('e') else ptte_start + gauss_start
         if ylog:
-            axs[i, j].set_yscale('log')
+            axs[i, j].axes.set_yscale('log', nonposy='clip')
         
         bd = best_params.copy()
         bd.pop(num)
@@ -76,7 +95,7 @@ for i, mea in enumerate(measures):
         for n, v in bd.items():
             ind = ind & (mdf[n] == v)
         _mdf = mdf[ind]
-        color = 0
+
         for c in categories:    
             ind = [True] * len(_mdf)
             cd = best_params.copy()
@@ -89,24 +108,29 @@ for i, mea in enumerate(measures):
             if c == 'ngrams':
                 iter_cv = best_ngrams
             elif c == 'Agent':
-               iter_cv = ['rdn'] 
+               iter_cv = ['rdn']
             else:
                 iter_cv = cdf[c].unique()
-
+                
             for cv in iter_cv:
-                #if cv == '2t3': st()
-                with warnings.catch_warnings(record=True) as w:
-                    warnings.simplefilter("always")
-                    cdf[cdf[c]==cv].plot(num, rwdt,
-                        label=c + '_' + cv, ax=axs[i, j], color=colors[color])
-                    if len(w) == 1:
-                        if w[0].category is UserWarning:
-                            axs[i, j].plot(
-                                cdf[cdf[c]==cv][num],
-                                cdf[cdf[c]==cv]['pReward'].values + 10e-200,
-                                color=colors[color])
+                if nng == 1 and c == 'ngrams':
+                    nam0 = 'Agent'
+                    nam1 = 'oie'
+                else:
+                    nam0 = c
+                    nam1 = cv
+                    
+                #if num == 'bw': st()
+                cdf[cdf[c]==cv].plot(num, rwdt,
+                    label=nam0 + '_' + nam1 + '_' + density,
+                    ax=axs[i, j],
+                    color=colors[color])
+
                 if j != len(axs[0, :]) - 1:
-                    axs[i, j].get_legend().remove()
+                    try:
+                        axs[i, j].get_legend().remove()
+                    except AttributeError:
+                        axs[i, j].legend().remove()
                 else:
                     axs[i, j].legend(loc='center left', bbox_to_anchor=(1, 0.5))
                 color += 1
